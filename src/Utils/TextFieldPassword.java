92b76bdf86cdcf8cb1fc96f99652d645ada2aa66
leet <robert.tarnow@maritz.com>
2018-01-05 22:32:08 -0600

    Initial commit

---
+++ src/Utils/TextFieldPassword.java
@@ -0,0 +1,47 @@
+package Utils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+
+public class TextFieldPassword extends JPasswordField {
+    private Shape shape;
+    private Color borderColor = UIUtils.COLOR_OUTLINE;
+
+    public TextFieldPassword() {
+        setOpaque(false);
+        setBackground(UIUtils.COLOR_BACKGROUND);
+        setForeground(Color.white);
+        setCaretColor(Color.white);
+        setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
+        setMargin(new Insets(2, 10, 2, 2));
+        setHorizontalAlignment(SwingConstants.LEFT);
+        setFont(UIUtils.FONT_GENERAL_UI);
+    }
+
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = UIUtils.get2dGraphics(g);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, UIUtils.ROUNDNESS, UIUtils.ROUNDNESS);
+        super.paintComponent(g2);
+    }
+
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2 = UIUtils.get2dGraphics(g);
+        g2.setColor(borderColor);
+        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, UIUtils.ROUNDNESS, UIUtils.ROUNDNESS);
+    }
+
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, UIUtils.ROUNDNESS, UIUtils.ROUNDNESS);
+        }
+        return shape.contains(x, y);
+    }
+
+    public void setBorderColor(Color color) {
+        borderColor = color;
+        repaint();
+    }
+}
---
+++ src/Utils/UIUtils.java
@@ -0,0 +1,27 @@
+package Utils;
+
+import java.awt.*;
+
+public class UIUtils {
+    public static final Font FONT_GENERAL_UI = new Font("Segoe UI", Font.PLAIN, 20);
+    public static final Font FONT_FORGOT_PASSWORD = new Font("Segoe UI", Font.PLAIN, 12);
+
+    public static final Color COLOR_OUTLINE = new Color(103, 112, 120);
+    public static final Color COLOR_BACKGROUND = new Color(37, 51, 61);
+    public static final Color COLOR_INTERACTIVE = new Color(108, 216, 158);
+    public static final Color COLOR_INTERACTIVE_DARKER = new Color(87, 171, 127);
+    public static final Color OFFWHITE = new Color(229, 229, 229);
+
+    public static final String BUTTON_TEXT_LOGIN = "Login";
+    public static final String BUTTON_TEXT_FORGOT_PASS = "Forgot your password?";
+
+    public static final String PLACEHOLDER_TEXT_USERNAME = "Username/email";
+
+    public static final int ROUNDNESS = 8;
+
+    public static Graphics2D get2dGraphics(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        return g2;
+    }
+}
